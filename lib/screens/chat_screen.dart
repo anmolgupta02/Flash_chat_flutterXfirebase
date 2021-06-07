3ecb465f9f9258cb286d742fcd131250808dd4a6
Anmol <anmolgupta02@gmail.com>
2021-06-08 01:52:57 +0530

    Implemented Bubble style to the text messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,11 @@
 import 'package:chat_firebase_application/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+final _firestore = Firestore.instance;
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -10,7 +13,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _firestore = Firestore.instance;
+  final messageTextEditingController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   FirebaseUser loggedInUser;
   String chat;
@@ -82,41 +85,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              /*This is for getting the source of data. In our case it is Stream which is sent by
-                firebase through _firestore*/
-              builder: (context, asyncSnapshot) {
-                /*this asyncSnapshot is property of flutter through which is it connecting the
-                  firebase stream and the widget builder stream*/
-                if (asyncSnapshot.hasData) {
-                  //the connection of Flutter Stream and Firebase Stream and the data is hold in messages.
-                  final messages = asyncSnapshot.data.documents;
-                  //Because we want to show the messages as a column containing list of data.
-                  List<Text> msgWidget = [];
-                  //Looping through the objects we got from firebase.
-                  for (var messages in messages) {
-                    //In firrbase to access the msg we need to have collections -> snapshot -> Document -> data -> Property
-                    final msgText = messages.data["text"];
-                    final messageSender = messages.data["sender"];
-                    //Creating Single Text Widgets from each objects
-                    final singleMsgWidget =
-                        Text('$msgText from $messageSender');
-                    //Adding this singleMsgWidget to the list.
-                    msgWidget.add(singleMsgWidget);
-                  }
-                  return Column(
-                    children: msgWidget,
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.lightBlueAccent,
-                    ),
-                  );
-                }
-              },
-            ),
+            MsgStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -124,6 +93,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: [
                   Expanded(
                     child: TextField(
+                      controller: messageTextEditingController,
                       autofocus: true,
                       onChanged: (value) {
                         chat = value;
@@ -134,6 +104,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      //This is used for clearing the text on the text field.
+                      messageTextEditingController.clear();
                       _firestore.collection('messages').add({
                         'text': chat,
                         'sender': loggedInUser.email,
@@ -150,6 +122,88 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// For modular code we separated the code from the above class.
+class MsgStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      /*This is for getting the source of data. In our case it is Stream which is sent by
+                firebase through _firestore*/
+      stream: _firestore.collection('messages').snapshots(),
+      /*this asyncSnapshot is property of flutter through which is it connecting the
+                  firebase stream and the widget builder stream*/
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.hasData) {
+          //the connection of Flutter Stream and Firebase Stream and the data is hold in messages.
+          final messages = asyncSnapshot.data.documents;
+          //Because we want to show the messages as a column containing list of data.
+          List<MessageBubble> msgWidget = [];
+          //Looping through the objects we got from firebase.
+          for (var messages in messages) {
+            //In firebase to access the msg we need to have collections -> snapshot -> Document -> data -> Property
+            final msgText = messages.data["text"];
+            final messageSender = messages.data["sender"];
+            //Creating Single Text Widgets from each objects
+            final singleMsgWidget =
+                MessageBubble(sender: messageSender, text: msgText);
+            //Adding this singleMsgWidget to the list.
+            msgWidget.add(singleMsgWidget);
+          }
+          //Using listView becuase we want to use scrolling effect.
+          return Expanded(
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              //Finally showing List of Text widget to the Column.
+              children: msgWidget,
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+//Designing the message body. To shape it like a bubble.
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text});
+
+  final String sender, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(fontSize: 12, color: Colors.grey),
+          ),
+          Material(
+            borderRadius: BorderRadius.circular(30.0),
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$text',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
